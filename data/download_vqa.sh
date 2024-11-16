f8ec186e794792000f4c7b742155c75dd557c8fa
mirodavide <filomenodavide.miro@gmail.com>
2024-11-16 21:54:49 +0100

    feat: download vqa dataset and gcp config

---
+++ data/download_vqa.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip
+unzip v2_Annotations_Train_mscoco.zip
+
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
+unzip v2_Annotations_Val_mscoco.zip
+
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
+unzip v2_Questions_Train_mscoco.zip
+
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip
+unzip v2_Questions_Val_mscoco.zip
+
+wget http://images.cocodataset.org/zips/train2014.zip
+unzip train2014.zip
+
+wget http://images.cocodataset.org/zips/val2014.zip
+unzip val2014.zip
