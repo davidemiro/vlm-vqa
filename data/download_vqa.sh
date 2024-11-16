8f2c9b1ed1120f8f67fc5178b59528b6976095f5
mirodavide <filomenodavide.miro@gmail.com>
2024-11-16 22:58:41 +0100

    feat: download vqa dataset and gcp config

--- data/download_vqa.sh
+++ data/download_vqa.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+.#!/bin/sh
 
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip
-unzip v2_Annotations_Train_mscoco.zip
+sudo wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip
+sudo unzip v2_Annotations_Train_mscoco.zip
 
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
-unzip v2_Annotations_Val_mscoco.zip
+sudo wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
+sudo unzip v2_Annotations_Val_mscoco.zip
 
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
-unzip v2_Questions_Train_mscoco.zip
+sudo wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
+sudo unzip v2_Questions_Train_mscoco.zip
 
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip
-unzip v2_Questions_Val_mscoco.zip
+sudo wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip
+sudo unzip v2_Questions_Val_mscoco.zip
 
-wget http://images.cocodataset.org/zips/train2014.zip
-unzip train2014.zip
+sudo wget http://images.cocodataset.org/zips/train2014.zip
+sudo unzip train2014.zip
 
-wget http://images.cocodataset.org/zips/val2014.zip
-unzip val2014.zip
+sudo wget http://images.cocodataset.org/zips/val2014.zip
+sudo unzip val2014.zip
