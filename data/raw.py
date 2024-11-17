664f19040592cf2ae88500a5a74367134f772ee3
mirodavide <filomenodavide.miro@gmail.com>
2024-11-17 17:29:37 +0100

    feat: use streamimg

--- data/raw.py
+++ data/raw.py
@@ -8,13 +8,13 @@ from transformers import DefaultDataCollator
 
 
 def get_dataset(annotations_path, questions_path):
-    annotations = datasets.load_dataset("json", data_files=annotations_path, field="annotations")
+    annotations = datasets.load_dataset("json", data_files=annotations_path, field="annotations", streaming=True)
     annotations = annotations["train"]
     annotations = pandas.DataFrame(annotations)
     annotations = annotations.drop(columns=['question_type', 'answers', 'answer_type', 'image_id'])
     annotations.rename(columns={'multiple_choice_answer': 'answer'}, inplace=True)
 
-    questions = datasets.load_dataset("json", data_files=questions_path, field="questions")
+    questions = datasets.load_dataset("json", data_files=questions_path, field="questions", streaming=True)
     questions = questions["train"]
     questions = pandas.DataFrame(questions)
 
