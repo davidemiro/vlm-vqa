a8ddcaec4fdac89e40d8aa3e386d7faa6761012b
mirodavide <filomenodavide.miro@gmail.com>
2024-11-17 16:13:11 +0100

    fix: remove multiprocessing

--- data/process.py
+++ data/process.py
@@ -3,7 +3,7 @@ from configs import configs
 from vlm.configuration_vlm import VLMConfig
 from vlm.processing_vlm import VLMProcessor
 from raw import RawDataCollator
-import multiprocessing
+
 
 config = configs.load_configs()["TRAIN"]
 
@@ -16,8 +16,8 @@ processor = VLMProcessor(vlm_config)
 data_collator_train = RawDataCollator(processor, config['train_img_path'], split='train')
 data_collator_val = RawDataCollator(processor, config['val_img_path'], split='val')
 
-dataset_train = dataset_train.map(data_collator_train, batched=False, num_proc=multiprocessing.cpu_count())
-dataset_val = dataset_val.map(data_collator_val, batched=False, num_proc=multiprocessing.cpu_count())
+dataset_train = dataset_train.map(data_collator_train, batched=False)
+dataset_val = dataset_val.map(data_collator_val, batched=False)
 
 dataset_train.save_to_disk(config['train_path'])
 dataset_val.save_to_disk(config['val_path'])
