f122f030111c0b2732bfa2cadaae5008b0c699ff
mirodavide <filomenodavide.miro@gmail.com>
2025-02-08 21:14:11 +0100

    feat: increase batch size, remove dataset limit

--- data/raw.py
+++ data/raw.py
@@ -9,13 +9,11 @@ from transformers import DefaultDataCollator
 
 def get_dataset(annotations_path, questions_path):
     annotations = datasets.load_dataset("json", data_files=annotations_path, field="annotations")
-    annotations = annotations["train"].select(range(10))
     annotations = pandas.DataFrame(annotations)
     annotations = annotations.drop(columns=['question_type', 'answers', 'answer_type', 'image_id'])
     annotations.rename(columns={'multiple_choice_answer': 'answer'}, inplace=True)
 
     questions = datasets.load_dataset("json", data_files=questions_path, field="questions")
-    questions = questions["train"].select(range(10))
     questions = pandas.DataFrame(questions)
 
     dataset = pandas.merge(questions, annotations, on="question_id")
@@ -31,9 +29,7 @@ class RawDataCollator(DefaultDataCollator):
 
     def _load_image(self, path, split, image_id):
         image_id = "0" * (12 - len(str(image_id))) + str(image_id)
-        #img = Image.open(os.path.join(path, "COCO_{}2014_{}.jpg".format(split, image_id)))
-        #TODO: remove
-        img = Image.open("COCO_val2014_000000581929.jpg")
+        img = Image.open(os.path.join(path, "COCO_{}2014_{}.jpg".format(split, image_id)))
 
         return img
 
