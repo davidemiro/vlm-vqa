fix: decrease batch size increase gradient accumulation steps

vlm/utils_vlm.py
@@ -43,7 +43,7 @@ def get_vlm(config):
     vlm_config = VLMConfig(text_length=int(config["text_length"]), num_patches=int(config["num_patches"]), visual_embed_dim=int(config["visual_embed_dim"]))
     processor = VLMProcessor(vlm_config, config['token'])
     vlm_model = VLMForCausalLM.from_pretrained("google/gemma-2-2b-it", config=vlm_config, attn_implementation="eager", torch_dtype=torch.bfloat16,
-                                                  token=config['token'], use_cache=False)
+                                                  token=config['token'])
     vlm_model.vit = Dinov2Model.from_pretrained("facebook/dinov2-base", config=vlm_config.vit_config, torch_dtype=torch.bfloat16)
 
     return processor, vlm_model, vlm_config