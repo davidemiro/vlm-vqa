db14ab025cb5da7b31b7d0ad59066f9e9b9e6bba
mirodavide <filomenodavide.miro@gmail.com>
2025-03-08 13:05:11 +0100

    fix: increase batch_size, reduce lora_rank

--- data/raw.py
+++ data/raw.py
@@ -9,13 +9,13 @@ from transformers import DefaultDataCollator
 
 def get_dataset(annotations_path, questions_path):
     annotations = datasets.load_dataset("json", data_files=annotations_path, field="annotations")
-    annotations = annotations["train"].select(range(10))
+    annotations = annotations["train"]
     annotations = pandas.DataFrame(annotations)
     annotations = annotations.drop(columns=['question_type', 'answers', 'answer_type', 'image_id'])
     annotations.rename(columns={'multiple_choice_answer': 'answer'}, inplace=True)
 
     questions = datasets.load_dataset("json", data_files=questions_path, field="questions")
-    questions = questions["train"].select(range(10))
+    questions = questions["train"]
     questions = pandas.DataFrame(questions)
 
     dataset = pandas.merge(questions, annotations, on="question_id")
