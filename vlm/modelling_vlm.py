276ac62f8d93e79b4f1021a2f5aee50a3dafe2a8
mirodavide <filomenodavide.miro@gmail.com>
2025-03-08 20:42:30 +0100

    feat: vit no grad

--- vlm/modelling_vlm.py
+++ vlm/modelling_vlm.py
@@ -6,6 +6,47 @@ from transformers import Gemma2ForCausalLM, HybridCache, Dinov2Model, Generation
 from transformers.modeling_outputs import CausalLMOutputWithPast, ModelOutput
 from vlm.configuration_vlm import VLMConfig
 
+class VLMForCausalLM(Gemma2ForCausalLM):
+    def __init__(self, config: VLMConfig):
+        super().__init__(config)
+        self.linear_projector = nn.Linear(config.visual_embed_dim, config.hidden_size)
+        self.vit = AutoModel.from_pretrained("facebook/dinov2-base")
+        self.vit.requires_grad = False
+        self.num_patches = config.num_patches
+
+        self.image_token_id = self.config.image_token_id
+        self.pad_token_id = self.config.pad_token_id
+
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[HybridCache] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        text_embeds: Optional[torch.FloatTensor] = None,
+        pixel_values: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        num_logits_to_keep: int = 0,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+
+
+        visual_embeds = self.vit(pixel_values)
+        visual_embeds = self.linear_projector(visual_embeds['last_hidden_state'])
+
+        input_ids = input_ids[:, self.num_patches:]
+
+        text_embeds = self.model.embed_tokens(input_ids)
+        input_embeds = torch.cat((text_embeds, visual_embeds), dim=1)
+
+        return super().forward(None, attention_mask, position_ids, past_key_values, input_embeds, labels, use_cache, output_attentions, output_hidden_states, return_dict, cache_position, num_logits_to_keep)
+
 
 @dataclass
 class VLMCausalLMOutputWithPast(ModelOutput):
@@ -45,47 +86,6 @@ class VLMCausalLMOutputWithPast(ModelOutput):
     hidden_states: Optional[Tuple[torch.FloatTensor]] = None
     attentions: Optional[Tuple[torch.FloatTensor]] = None
     image_hidden_states: Optional[torch.FloatTensor] = None
-
-class VLMForCausalLM(Gemma2ForCausalLM):
-    def __init__(self, config: VLMConfig):
-        super().__init__(config)
-        self.linear_projector = nn.Linear(config.visual_embed_dim, config.hidden_size)
-        self.vit = AutoModel.from_pretrained("facebook/dinov2-base")
-        self.num_patches = config.num_patches
-
-        self.image_token_id = self.config.image_token_id
-        self.pad_token_id = self.config.pad_token_id
-
-
-    def forward(
-        self,
-        input_ids: torch.LongTensor = None,
-        attention_mask: Optional[torch.Tensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        past_key_values: Optional[HybridCache] = None,
-        inputs_embeds: Optional[torch.Tensor] = None,
-        text_embeds: Optional[torch.FloatTensor] = None,
-        pixel_values: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
-        use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-        cache_position: Optional[torch.LongTensor] = None,
-        num_logits_to_keep: int = 0,
-    ) -> Union[Tuple, CausalLMOutputWithPast]:
-
-
-        visual_embeds = self.vit(pixel_values)
-        visual_embeds = self.linear_projector(visual_embeds['last_hidden_state'])
-
-        input_ids = input_ids[:, self.num_patches:]
-
-        text_embeds = self.model.embed_tokens(input_ids)
-        input_embeds = torch.cat((text_embeds, visual_embeds), dim=1)
-
-        return super().forward(None, attention_mask, position_ids, past_key_values, input_embeds, labels, use_cache, output_attentions, output_hidden_states, return_dict, cache_position, num_logits_to_keep)
-
 
 class VLMForConditionalGeneration(VLMForCausalLM, GenerationMixin):
 
