67a5ad3b0811fe741f41a9e9f3a6b313daf49654
mirodavide <filomenodavide.miro@gmail.com>
2025-03-12 23:27:28 +0100

    feat: develop cumpute_accuracy

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ def main():
         bf16=True,
         bf16_full_eval=True,
         ddp_find_unused_parameters=False,
-        eval_accumulation_steps=8,
+        eval_accumulation_steps=16,
         gradient_accumulation_steps=int(config["gradient_accumulation_steps"]),
         batch_eval_metrics=True
 
