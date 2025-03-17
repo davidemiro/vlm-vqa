24e363e8d3d8eea984dc67b5043495092211a8c8
mirodavide <filomenodavide.miro@gmail.com>
2025-03-18 00:10:01 +0100

    feat: final traing

--- train.py
+++ train.py
@@ -60,9 +60,9 @@ def main():
         output_dir=config["output_dir"],
         evaluation_strategy="steps",  # Evaluate at the end of each epoch
         save_strategy="steps",
-        eval_steps=len(dataset_train) // config['train_batch_size'] * config['gradient_accumulation_steps'],
-        save_steps=len(dataset_train) // config['train_batch_size'] * config['gradient_accumulation_steps'],
-        torch_empty_cache_steps=len(dataset_train) // config['train_batch_size'] * config['gradient_accumulation_steps'],
+        eval_steps=len(dataset_train) // config['batch_size'] * config['gradient_accumulation_steps'],
+        save_steps=len(dataset_train) // config['batch_size'] * config['gradient_accumulation_steps'],
+        torch_empty_cache_steps=len(dataset_train) // config['batch_size'] * config['gradient_accumulation_steps'],
         learning_rate=float(config["learning_rate"]),
         weight_decay=float(config["weight_decay"]),
         per_device_train_batch_size=int(config["batch_size"]),
