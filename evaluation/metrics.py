e6b74a091f80324ab7652e5d16ea8c66e20d2e2f
mirodavide <filomenodavide.miro@gmail.com>
2025-03-17 21:56:12 +0100

    feat: use percentage dataset

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,16 +1,18 @@
 import evaluate
 import os
+import torch
 accuracy = evaluate.load("accuracy")
 
 
 def compute_accuracy(p, compute_result=False):
+    gpu_rank = torch.cuda.current_device()
 
     print("Computing accuracy... {}".format(compute_result))
     if compute_result:
         total_sum = 0
         count = 0
 
-        with open("store_values", 'r') as file:
+        with open("store_values_{}".format(gpu_rank), 'r') as file:
             for line in file:
                 try:
                     
@@ -23,9 +25,12 @@ def compute_accuracy(p, compute_result=False):
             if count == 0:
                 return 0
 
-            os.remove("store_values")
+            os.remove("store_values_{}".format(gpu_rank))
+
 
             mean = total_sum / count
+
+            print("Mean {}: {}".format(gpu_rank, mean))
         return {"accuracy" : mean}
 
     else:
