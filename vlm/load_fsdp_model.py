e692f62b8e0099b43bd8c122c0db1ad308443d21
mirodavide <filomenodavide.miro@gmail.com>
2025-03-29 18:48:46 +0100

    feat: load fsdp model

--- vlm/load_fsdp_model.py
+++ vlm/load_fsdp_model.py
@@ -5,39 +5,31 @@ from torch.distributed.checkpoint import (
     FileSystemReader,
 )
 
+import torch.distributed as dist
+
 from torch.distributed.fsdp import (
     FullyShardedDataParallel as FSDP,
     StateDictType
 )
 
 import torch.distributed.checkpoint as dist_cp
+import os
+
+# Manually set RANK and WORLD_SIZE for a single process
+os.environ["RANK"] = "0"
+os.environ["WORLD_SIZE"] = "1"
+os.environ["MASTER_ADDR"] = "localhost"  # Required for init
+os.environ["MASTER_PORT"] = "12355"
 
 model_name = "mirodavide/vlm-vqa"
 model_path = "vlm_checkpoint/"
 
 
-
-def from_fsdp_shards_to_hf(model, model_path):
-
-    state_dict = {
-        "model": model.state_dict()
-    }
-
-    dist_cp.load_state_dict(
-        state_dict=state_dict,
-        storage_reader=FileSystemReader(model_path),
-        no_dist=True,
-    )
-
-    model.load_state_dict(state_dict["model"])
-
-    print(f"Sharded state checkpoint loaded from {model_path}")
-    return model
-
-
 def load_model_sharded(model, rank, path):
 
     reader = FileSystemReader(path)
+
+    dist.init_process_group("nccl", rank=rank, world_size=1)
 
     with FSDP.state_dict_type(model, StateDictType.SHARDED_STATE_DICT):
         checkpoint = {"model": model.state_dict()}
@@ -45,9 +37,13 @@ def load_model_sharded(model, rank, path):
             ck = checkpoint.keys()
             print(f" checkpoint key len = {len(ck)} and \n keys =  {ck}")
 
+
         dist_cp.load_state_dict(
             state_dict=checkpoint,
             storage_reader=reader,
+            process_group=dist.group.WORLD,
+            coordinator_rank=0,
+            no_dist=False,
         )
         if rank == 0:
             print(f"checkpoint after load_state_dict()")
@@ -66,6 +62,7 @@ vlm_model = VLMForCausalLM(vlm_config)
 load_model_sharded(vlm_model, 4, model_path)
 
 vlm_model.save_pretrained("vlm-vqa-1.0")
+vlm_model.push_to_hub()
 
 
 
