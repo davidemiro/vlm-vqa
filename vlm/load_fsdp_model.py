5ce28ce953c3211f88693c10b71a10d5e35b2cb0
mirodavide <filomenodavide.miro@gmail.com>
2025-03-31 22:46:54 +0200

    fix: helm command

--- vlm/load_fsdp_model.py
+++ vlm/load_fsdp_model.py
@@ -12,29 +12,24 @@ model_path = "/content/drive/MyDrive/train_vlm_vqa_1e05_lr"
 
 
 def load_model_sharded(model, model_path):
-    print("E")
     state_dict = {
         "model": model.state_dict(),
     }
 
-    # since no progress group is initialized, DCP will disable any collectives.
     dist_cp.load(
         state_dict=state_dict,
         checkpoint_id=model_path,
     )
-    print("F")
     model.load_state_dict(state_dict["model"])
-    print("G")
 
     return model
 
-print("A")
+
 vlm_config = VLMConfig(text_length=32, num_patches=257, visual_embed_dim=768)
-print("B")
 vlm_model = VLMForCausalLM(vlm_config)
-print("C")
+
 load_model_sharded(vlm_model, model_path)
-print("D")
+
 vlm_model.save_pretrained("/content/drive/MyDrive/train_vlm_vqa_1e05_lr/vlm-vqa-1.0")
 vlm_model.push_to_hub(repo_id=model_name)
 
