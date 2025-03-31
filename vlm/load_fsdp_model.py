5750c109edfffe06b135e2de89b8f25fe0f07ce8
mirodavide <filomenodavide.miro@gmail.com>
2025-03-31 20:58:51 +0200

    feat: load fsdp model

--- vlm/load_fsdp_model.py
+++ vlm/load_fsdp_model.py
@@ -12,6 +12,7 @@ model_path = "/content/drive/MyDrive/train_vlm_vqa_1e05_lr"
 
 
 def load_model_sharded(model, model_path):
+    print("E")
     state_dict = {
         "model": model.state_dict(),
     }
@@ -21,16 +22,19 @@ def load_model_sharded(model, model_path):
         state_dict=state_dict,
         checkpoint_id=model_path,
     )
+    print("F")
     model.load_state_dict(state_dict["model"])
+    print("G")
 
     return model
 
-
+print("A")
 vlm_config = VLMConfig(text_length=32, num_patches=257, visual_embed_dim=768)
+print("B")
 vlm_model = VLMForCausalLM(vlm_config)
-
+print("C")
 load_model_sharded(vlm_model, model_path)
-
+print("D")
 vlm_model.save_pretrained("vlm-vqa-1.0")
 vlm_model.push_to_hub()
 
