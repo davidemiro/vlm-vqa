560a990d378160f98fc3fa741279399b00898689
mirodavide <filomenodavide.miro@gmail.com>
2025-06-14 16:17:28 +0200

    fix: processing error

--- train.py
+++ train.py
@@ -11,7 +11,9 @@ import multiprocessing as mp
 
 def main():
 
-    torch.set_default_dtype(torch.float16)
+
+
+    torch.set_default_dtype(torch.float32)
 
     config = configs.load_configs()["TRAIN"]
 
@@ -32,9 +34,11 @@ def main():
 
     processor, vlm_model, vlm_config = get_vlm(config)
 
+
     if to_bool(config["lora"]):
         lora_config = LoraConfig(
             r=int(config['lora_rank']),
+            target_modules= ["q_proj", "k_proj", "v_proj", "out_proj", "fc_in", "fc_out", "wte"],
             lora_alpha=int(config['lora_alpha']),
             lora_dropout=float(config['lora_dropout']),
             bias=config['lora_bias'],
@@ -46,6 +50,8 @@ def main():
         vlm_model = lora_model
 
     data_collator_batch = BatchDataCollator(processor)
+
+    vlm_model.to("mps")
 
     training_args = TrainingArguments(
         output_dir=config["output_dir"],
@@ -66,7 +72,7 @@ def main():
         logging_steps=1,
         logging_dir="./logs",
         save_total_limit=1,
-        fp16=True,
+        fp16=False,
         fp16_full_eval=True,
         ddp_find_unused_parameters=False,
         eval_accumulation_steps=int(config["eval_accumulation_steps"]),
